40ce988414f85103bf86d48a1a770e36aceaa0a0
zbtrs <1339623011@qq.com>
2022-08-21 13:24:43 +0800

    基本完成，接下来处理一些细节问题

--- src/Sudoku.cpp
+++ src/Sudoku.cpp
@@ -105,7 +105,7 @@ string getCnf(int a[][COL],int b[][COL]) {
         cout << "Error! Can't open file" << endl;
         exit(-1);
     }
-    int tot = 0;
+    int tot = 0,cnt = 0;
 
     for (int i = 0; i < ROW; i++) {
         for (int j = 0; j < COL; j++) {
@@ -117,16 +117,16 @@ string getCnf(int a[][COL],int b[][COL]) {
             }
         }
     }
-    fos << "p cnf 1458 " << 17820 + tot << " " << endl;
+    fos << "p cnf 1458 " << 29970 + tot << " " << endl;
     //TODO 计算有多少个子句
     //对于已经填了的，直接生成单语句
     for (int i = 0; i < ROW; i++) {
         for (int j = 0; j < COL; j++) {
             if (a[i][j] != 0) {
-                fos << (i + 1) * 100 + (j + 1) * 10 + a[i][j] << " " << 0 << endl;
+                fos << i * 81 + j * 9 + a[i][j] << " " << 0 << endl;
             }
             if (b[i][j] != 0) {
-                fos << 1000 + (i + 1) * 100 + (j + 1) * 10 + b[i][j] << " " << 0 << endl;
+                fos << 729 + i * 81 + j * 9 + b[i][j] << " " << 0 << endl;
             }
         }
     }
@@ -134,17 +134,115 @@ string getCnf(int a[][COL],int b[][COL]) {
     for (int i = 1; i <= 9; i++) {
         for (int j = 1; j <= 9; j++) {
             for (int k = 1; k <= 9; k++) {
-                fos << i * 100 + j * 10 + k << " ";
+                fos << (i - 1) * 81 + (j - 1) * 9 + k << " ";
             }
             fos << 0 << endl;
+            cnt++;
             for (int k = 1; k <= 9; k++) {
-                fos << 1000 + i * 100 + j * 10 + k << " ";
-
+                fos << 729 + (i - 1) * 81 + (j - 1) * 9 + k << " ";
             }
             fos << 0 << endl;
-          //  cnt += 2;
+            cnt++;
+            for (int k = 1; k <= 9; k++) {
+                for (int p = k + 1; p <= 9; p++) {
+                    fos << -((i - 1) * 81 + (j - 1) * 9 + k) << " " << -((i - 1) * 81 + (j - 1) * 9 + p) << " " << 0 << endl;
+                    fos << -(729 + (i - 1) * 81 + (j - 1) * 9 + k) << " " << -(729 + (i - 1) * 81 + (j - 1) * 9 + p) << " " << 0 << endl;
+                    cnt += 2;
+                }
+            }
+            //cnt += 2;
         }
     }
+    //行
+    for (int i = 1; i <= 9; i++) {
+        for (int k = 1; k <= 9; k++) {
+            for (int j = 1; j <= 9; j++) {
+                fos << ((i - 1) * 81 + (j - 1) * 9 + k) << " ";
+            }
+            fos << 0 << endl;
+            for (int j = 1; j <= 9; j++) {
+                fos << (729 + (i - 1) * 81 + (j - 1) * 9 + k) << " ";
+            }
+            fos << 0 << endl;
+            cnt += 2;
+            for (int p = 1; p <= 9; p++) {
+                for (int q = p + 1; q <= 9; q++) {
+                    fos << -((i - 1) * 81 + (p - 1) * 9 + k) << " " << -((i - 1) * 81 + (q - 1) * 9 + k) << " " << 0 << endl;
+                    fos << -(729 + (i - 1) * 81 + (p - 1) * 9 + k) << " " << -(729 + (i - 1) * 81 + (q - 1) * 9 + k) << " " << 0 << endl;
+                    cnt += 2;
+                }
+            }
+        }
+    }
+    //列
+    for (int j = 1; j <= 9; j++) {
+        for (int k = 1; k <= 9; k++) {
+            for (int i = 1; i <= 9; i++) {
+                fos << ((i - 1) * 81 + (j - 1) * 9 + k) << " ";
+            }
+            fos << 0 << endl;
+            for (int i = 1; i <= 9; i++) {
+                fos << (729 + (i - 1) * 81 + (j - 1) * 9 + k) << " ";
+            }
+            fos << 0 << endl;
+            cnt += 2;
+            for (int p = 1; p <= 9; p++) {
+                for (int q = p + 1; q <= 9; q++) {
+                    fos << -((p - 1) * 81 + (j - 1) * 9 + k) << " " << -((q - 1) * 81 + (j - 1) * 9 + k) << " " << 0 << endl;
+                    fos << -(729 + (p - 1) * 81 + (j - 1) * 9 + k) << " " << -(729 + (q - 1) * 81 + (j - 1) * 9 + k) << " " << 0 << endl;
+                    cnt += 2;
+                }
+            }
+        }
+    }
+    //3*3
+    for (int x = 0; x < 3; x++) {
+        for (int y = 0; y < 3; y++) {
+            int x1 = x * 3 + 1,x2 = x * 3 + 3;
+            int y1 = y * 3 + 1,y2 = y * 3 + 3;
+            for (int k = 1; k <= 9; k++) {
+                for (int i = x1; i <= x2; i++) {
+                    for (int j = y1; j <= y2; j++) {
+                        fos << (i - 1) * 81 + (j - 1) * 9 + k << " ";
+                    }
+                }
+                fos << 0 << endl;
+                for (int i = x1; i <= x2; i++) {
+                    for (int j = y1; j <= y2; j++) {
+                        fos << 729 + (i - 1) * 81 + (j - 1) * 9 + k << " ";
+                    }
+                }
+                fos << 0 << endl;
+                cnt += 2;
+
+                for (int i = x1; i <= x2; i++) {
+                    for (int j = y1; j <= y2; j++) {
+                        for (int p = x1; p <= x2; p++) {
+                            for (int q = y1; q <= y2; q++) {
+                                if (i == p && j == q) {
+                                    continue;
+                                }
+                                fos << -((i - 1) * 81 + (j - 1) * 9 + k) << " " << -((p - 1) * 81 + (q - 1) * 9 + k) << " " << 0 << endl;
+                                fos << -(729 + (i - 1) * 81 + (j - 1) * 9 + k) << " " << -(729 + (p - 1) * 81 + (q - 1) * 9 + k) << " " << 0 << endl;
+                                cnt += 2;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //公共部分
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 1; k <= 9; k++) {
+                fos << 0 - ((i + 6) * 81 + (j + 6) * 9 + k) << " " << (729 + i * 81 + j * 9 + k) << " " << 0 << endl;
+                fos << ((i + 6) * 81 + (j + 6) * 9 + k) << " " << 0 - (729 + i * 81 + j * 9 + k) << " " << 0 << endl;
+                cnt += 2;
+            }
+        }
+    }
+    /*
     //行
     for (int y = 1; y <= 9; y++) {
         for (int z = 1; z <= 9; z++) {
@@ -206,16 +304,7 @@ string getCnf(int a[][COL],int b[][COL]) {
             }
         }
     }
-    //公共部分
-    for (int i = 1; i <= 3; i++) {
-        for (int j = 1; j <= 3; j++) {
-            for (int k = 1; k <= 9; k++) {
-                fos << 0 - ((i + 6) * 100 + (j + 6) * 10 + k) << " " << (1000 + i * 100 + j * 10 + k) << " " << 0 << endl;
-                fos << ((i + 6) * 100 + (j + 6) * 10 + k) << " " << 0 - (1000 + i * 100 + j * 10 + k) << " " << 0 << endl;
-                //cnt += 2;
-            }
-        }
-    }
+     */
     //cout << cnt << endl;
     fos.close();
     return "sudoku.cnf";
@@ -240,46 +329,3 @@ string initSudoku() {
     return getCnf(sudoku1,sudoku2);
 }
 
-void printCompleteSudoku(int *result, int varNum) {
-    int a[9][9] = {0},b[9][9] = {0};
-    for (int i = 0; i < varNum / 2; i++) {
-        if (result[i] > 0) {
-            int x = result[i] / 100 - 1;
-            int y = (result[i] - (x + 1) * 100) / 10 - 1;
-            a[x][y] = result[i] - (x + 1) * 100 - (y + 1) * 10;
-        }
-    }
-    for (int i = varNum / 2; i < varNum; i++) {
-        if (result[i] > 0) {
-            int t = result[i] - 1000;
-            int x = t / 100 - 1;
-            int y = (t - (x + 1) * 100) / 10 - 1;
-            b[x][y] = t - (x + 1) * 100 - (y + 1) * 10;
-        }
-    }
-    for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 9; j++) {
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
-    }
-    for (int i = 6; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            cout << a[i][j] << " ";
-        }
-        for (int j = 3; j < 9; j++) {
-            cout << b[i - 6][j] << " ";
-        }
-        cout << endl;
-    }
-    for (int i = 3; i < 9; i++) {
-        for (int j = 1; j <= 12; j++) {
-            cout << " ";
-        }
-        for (int j = 0; j < 9; j++) {
-            cout << b[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-}
--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@ void terminalTips() {
     printf("------------------------------------------------------------------------\n");
 }
 
-void printAnswer(clock_t startTime,clock_t endTime,bool flag,int *result,int varNum) {
-    string outputFile = filePath + "solution.res";
+void printAnswer(clock_t startTime,clock_t endTime,bool flag,int *result,int varNum,string filename) {
+    string outputFile = filePath + filename;
     ofstream fos(outputFile);
     if (!fos.is_open()) {
         cout << "error! Can't open a new file\n" << endl;
@@ -39,6 +39,51 @@ void printAnswer(clock_t startTime,clock_t endTime,bool flag,int *result,int var
     fos.close();
 }
 
+void printCompleteSudoku(int *result, int varNum) {
+    int a[9][9] = {0},b[9][9] = {0};
+    for (int i = 0; i < varNum / 2; i++) {
+        if (result[i] > 0) {
+            int t = i;
+            int x = t / 81;
+            int y = (t - x * 81) / 9;
+            a[x][y] = t - x * 81 - y * 9 + 1;
+        }
+    }
+    for (int i = varNum / 2; i < varNum; i++) {
+        if (result[i] > 0) {
+            int t = i - 729;
+            int x = t / 81;
+            int y = (t - x * 81) / 9;
+            b[x][y] = t - x * 81 - y * 9 + 1;
+        }
+    }
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    for (int i = 6; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << a[i][j] << " ";
+        }
+        for (int j = 3; j < 9; j++) {
+            cout << b[i - 6][j] << " ";
+        }
+        cout << endl;
+    }
+    for (int i = 3; i < 9; i++) {
+        for (int j = 1; j <= 12; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < 9; j++) {
+            cout << b[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+}
+
 int main() {
     terminalTips();
     int choice = 0;
@@ -46,35 +91,36 @@ int main() {
     while (choice) {
         if (choice == 1) {
             string filename = initSudoku();
+
             int varnum = 0;
             Head* LinkedList = initCnf(varnum,filename);
             int *result = new int[varnum];
+            memset(result,0,sizeof(int) * varnum);
             clock_t startTime,endTime;
             startTime = clock();
             bool flag = DPLL(LinkedList,result);
             endTime = clock();
-            printAnswer(startTime,endTime,flag,result,varnum);
+            printAnswer(startTime,endTime,flag,result,varnum,"sudoku.res");
             printCompleteSudoku(result,varnum);
+
 
             //TODO: Sudoku
         } else if (choice == 2) {
             //TODO 将文件操作放到配置文件或者环境变量中
             int varnum;
-            string file = "problem1-20.cnf";
-            Head* LinkedList = initCnf(varnum,file);
-            int result[varnum];
+            string file = "problem1-20";
+            cin >> file;
+            string suffix = ".cnf";
+            string filename = file + suffix;
+            Head* LinkedList = initCnf(varnum,filename);
+            int *result = new int[varnum];
+            memset(result,0,sizeof(int) * varnum);
             clock_t startTime,endTime;
-            string outputFile = filePath + "solution.res";
-            ofstream fos(outputFile);
-            if (!fos.is_open()) {
-                cout << "error! Can't open a new file\n" << endl;
-                exit(-1);
-            }
+            string outputFile = file + ".res";
             startTime = clock();
             bool flag = DPLL(LinkedList,result);
             endTime = clock();
-            printAnswer(startTime,endTime,flag,result,varnum);
-
+            printAnswer(startTime,endTime,flag,result,varnum,outputFile);
 
         } else if (choice == 0) {
             break;
@@ -82,7 +128,7 @@ int main() {
             cout << "Please input the right num! " << endl;
             exit(-1);
         }
-        cout << "求解已完成，请输入下一条指令: " << endl;
+        terminalTips();
         cin >> choice;
     }
 
